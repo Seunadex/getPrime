f153c8156bcdd6ce26688a9aab7f69615f83d374
Adekunle Seun <adekunleseun001@gmail.com>
2017-05-08 21:13:42 +0100

    Add more test cases

--- spec/getPrimeSpec.js
+++ spec/getPrimeSpec.js
@@ -7,6 +7,18 @@ describe("testing for proper functionality", function(){
 			expect(getPrimes.getPrimes(5)).toEqual([2, 3, 5]);
 		});
 
+		it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83 ] for primes numbers between 0 and 88", function(){
+			expect(getPrimes.getPrimes(88)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83 ]);
+		});
+
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31] for prie numbers between 0 and 35", function(){
+			expect(getPrimes.getPrimes(35)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31])
+		});
+
+		it("should return [2, 3, 5, 7] for primes numbers between 0 and 8", function(){
+			expect(getPrimes.getPrimes(8)).toEqual([2, 3, 5, 7]);
+		});
+
 		it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23 ] for primes numbers between 0 and 23", function(){
 			expect(getPrimes.getPrimes(23)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23 ]);
 		});
@@ -15,13 +27,15 @@ describe("testing for proper functionality", function(){
 			expect(getPrimes.getPrimes(15)).toEqual([ 2, 3, 5, 7, 11, 13 ]);
 		});
 
-		it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83 ] for primes numbers between 0 and 88", function(){
-			expect(getPrimes.getPrimes(88)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83 ]);
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73] for primes numbers between 0 and 73", function(){
+			expect(getPrimes.getPrimes(73)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73]);
 		});
 
-		it("should return [2, 3, 5, 7] for primes numbers between 0 and 8", function(){
-			expect(getPrimes.getPrimes(8)).toEqual([2, 3, 5, 7]);
+		it("should return [2, 3] for primes numbers between 0 and 3", function(){
+			expect(getPrimes.getPrimes(3)).toEqual([2, 3]);
 		});
+
+
 	})
 
 	describe("case for invalid input", function(){
