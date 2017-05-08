61d84a0dc364bb00860df9f24e4ccd8574355965
Adekunle Seun <adekunleseun001@gmail.com>
2017-05-08 20:30:46 +0100

    Add function codes

--- app/getPrimes.js
+++ app/getPrimes.js
@@ -1,1 +1,42 @@
 'use strict'
+const getPrimes = (range) => {
+	let primes = [2, 3];
+	const checkPrime = (range) => {
+		let rangeIsPrime;
+		let counter;
+		for(counter = 2; counter < range; counter++) {
+			if(rangeIsPrime % counter === 0) {
+				rangeIsPrime = false;
+				break;
+			}
+			else{
+				rangeIsPrime = true;
+			}
+		}
+		return rangeIsPrime;
+	};
+	if (range === 1) {
+		return "1 is not a prime number";
+	}
+	else if (range === 2) {
+		return [range];
+	}
+	else if (range === 3) {
+		return [range];
+	}
+	else if(range <= 0) {
+		return "Negative numbers cannot be a prime";
+	}
+	else {
+		let i;
+		for(i = 4; i<= range; i++) {
+			if(checkPrime(i) === true) {
+				primes.push(i);
+			}
+		}
+		return primes;
+	}
+}
+module.exports = {
+	getPrimes: getPrimes
+};
